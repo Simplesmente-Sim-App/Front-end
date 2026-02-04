a6fc58142a93174137c709258df327a63448bba7
Ian de Aquino Bianquini <ian.aquino2003@gmail.com>
2026-02-04 12:45:42 -0300

    chore: setup configs for project

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-vercel';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: { adapter: adapter() },
-	preprocess: [mdsvex()],
+	preprocess: [mdsvex(), sveltePreprocess({
+		defaults: {
+			style: 'scss',
+			script: 'typescript'
+		}
+	})],
 	extensions: ['.svelte', '.svx']
 };
 
